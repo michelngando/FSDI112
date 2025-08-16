054d6646729940efcb4a2a14e5ca5f18f0fca693
Michel Ngando <michelngando81@gmail.com>
2025-08-16 14:01:37 -0500

    class #3 v1

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
-    PostListView,
-    PostDetailView
+    PostListView   
 )
 
 urlpatterns = [
-    path('list/', PostListView.as_view(), "name=post_list"),
+    path('list/', PostListView.as_view(), name="post_list"),
+    path('new/', PostCreateView.as_view(), name="new_post")
     path('<int:pk>/', PostDetailView.as_view(), name="post_detail")
+    path("int:pk>/delete/", PostDeleteView.as_view(), name="post_delete")
+    path('<int:pk>/edit/', PostUpdateView.as_view(), name= "post_update")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, DeleteView
-from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from .models import Post, Status
+from django.urls import reverse_lazy
+from django
 # Create your views here.
 
 
@@ -13,13 +15,66 @@ class PostListView(ListView):
     #context is a python dictionary that holds the data for the generic view
     #and this context travels to the htmls
     #by default the context name of ListView and DetailView is 
-    context_object_name="posts_list"
+    context_object_name="post_list"
 
-class PostDetailView(DetailView):
-    template_name="posts/detail.html"
-    model=Post
-    context_object_name="single_post"
+    def get_queryset(self):
+        status = Status.objects.get(name="published")
+        return Post.objects.filter(status=status).order_by("created_on").reverse()
+
+   
+   
 
 class PostCreateView(CreateView):
     template_name = "posts/new.html"
-    model=Post
+    model = Post
+    fields=['title', 'subtitle', 'body', 'status']
+
+    def form_valid(self, form):
+        print_form = f"""
+        -----------------------------------------
+        {form}
+        -----------------------------------------
+        {form.instance}
+        """
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+
+
+class PostDetailView(DetailView):
+    template_name="posts/detail.html"
+    model= Post
+    context_object_name="single_post"
+
+    def test_func(self):
+        post = self.get_object()
+        if post.status.name == "archived":
+            if self.request.user.is_authenticated:
+                return True
+            
+
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        selected_post = self.object
+        ststus = status.objects.get(name="published")
+        context['prev_post'] = Post.objects.filter(id_lt=selected_post.id)\
+                                            .filter(status=status)\
+                                            .order_by('-created_on')\
+                                            .first()
+        context['next_post'] = Post.objects.filter(id_gt=see.id)\
+                                            .filter(status=status)\
+                                            .order_by('created_on')\
+                                            .first()
+    return context
+
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields=['title', 'subtitle', 'body', 'status']
+   
+        #form.instance.author = self.request.user
+        #return super().form_valid(form)
